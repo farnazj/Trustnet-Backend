a8f3ace89cb79772f8d521049dc574440b8629ad
farnazj <farnaz.jahanbakhsh92@gmail.com>
2018-10-22 14:12:38 -0400

    session saved after login to avoid session.save() and store.get() race

---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+const passport = require('passport');
+var routeHelpers = require('../lib/routeHelpers');
+var authController = require('../controllers/authcontroller.js');
+
+
+router.route('/login')
+.post(passport.authenticate('local-login'), function(req, res){
+  req.session.save(() => {
+       res.redirect('/');
+     })
+});
+
+router.route('/logout')
+.post(routeHelpers.isLoggedIn, function(req, res){
+  req.logout();
+  res.redirect('/login');
+});
+
+router.route('/signup')
+.get( authController.signup)
+.post( passport.authenticate('local-signup', {
+        successRedirect: '/',
+        failureRedirect: '/signup',
+        failureFlash: true
+    }
+))
+
+
+module.exports = router;
