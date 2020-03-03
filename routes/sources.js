84dc6db3f921f80ca2a27c61ac09fa9e3999bee2
farnazj@mit.edu <farnaz.jahanbakhsh92@gmail.com>
2020-03-03 16:07:44 -0500

    updated get sources route

--- routes/sources.js
+++ routes/sources.js
@@ -10,22 +10,55 @@ var kue = require('kue')
 
 router.route('/sources')
 
-.get(wrapAsync(async function(req, res) {
+.get(routeHelpers.isLoggedIn, wrapAsync(async function(req, res) {
 
   let paginationReq = routeHelpers.getLimitOffset(req);
   let searchTerm = req.headers.searchterm ? req.headers.searchterm : '';
 
-  let sources = await db.Source.findAll({
-    where: {
-      [Op.or]: [
-        db.sequelize.where(db.sequelize.fn('concat', db.sequelize.col('firstName'), ' ', db.sequelize.col('lastName')), {
-          [Op.like]: '%' + searchTerm + '%'
-        }),
+  let whereClause = {
+    [Op.or]: [
+      db.sequelize.where(db.sequelize.fn('concat', db.sequelize.col('firstName'), ' ', db.sequelize.col('lastName')), {
+        [Op.like]: '%' + searchTerm + '%'
+      }),
+        {
+          userName: { [Op.like]: '%' + searchTerm + '%' }
+        }
+    ]
+  }
+
+  if (req.headers.followconstraint) {
+
+    let authUser = await db.Source.findByPk(req.user.id);
+    let followedIds = (await authUser.getFollows()).map(el => el.id);
+
+    if (req.headers.followconstraint == 'followed') {
+      whereClause = {
+        [Op.and]: [
+          whereClause,
           {
-            userName: { [Op.like]: '%' + searchTerm + '%' }
+            id: {
+              [Op.in]: followedIds
+            }
           }
-      ]
-    },
+        ]
+      }
+    }
+    else if (req.headers.followconstraint == 'not followed') {
+      whereClause = {
+        [Op.and]: [
+          whereClause,
+          {
+            id: {
+              [Op.notIn]: followedIds
+            }
+          }
+        ]
+      }
+    }
+  }
+
+  let sources = await db.Source.findAll({
+    where: whereClause,
     ...paginationReq
   });
 
