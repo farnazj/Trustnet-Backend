9e06347e8cbd4b7943b6b1a4d6e453c65699b612
Farnaz Jahanbakhsh <farnaz.jahanbakhsh92@gmail.com>
2021-03-26 17:27:54 -0500

    added migrations for standalonetitles

--- models/post.js
+++ models/post.js
@@ -41,7 +41,7 @@ module.exports = (sequelize, DataTypes) => {
     models.Post.hasMany(models.Assessment, {as: 'PostAssessments'});
     models.Post.hasMany(models.CustomTitle, {as: 'PostCustomTitles'});
     models.Post.belongsToMany(models.Tag, {through: 'PostTags'});
-
+    models.Post.hasOne(models.StandaloneTitle);
   };
 
   return Post;
---
+++ models/standaloneTitle.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const StandaloneTitle = sequelize.define('StandaloneTitle', {
+    text: {
+      type: DataTypes.TEXT('medium')
+    },
+    hash: {
+      type: DataTypes.STRING
+    }
+  }, {
+    charset: 'utf8mb4',
+  });
+
+
+  StandaloneTitle.associate = function (models) {
+    models.StandaloneTitle.hasMany(models.CustomTitle, {as: 'StandaloneCustomTitles'});
+    models.StandaloneTitle.belongsTo(models.Post);
+  };
+
+  return StandaloneTitle;
+};
