59eba4002864faec4aae72a5c842a6dbe471721f
Farnaz Jahanbakhsh <farnaz.jahanbakhsh92@gmail.com>
2021-04-09 16:24:43 -0500

    preferences bug fixes

--- routes/preferences.js
+++ routes/preferences.js
@@ -9,7 +9,7 @@ router.route('/preferences/:source_id')
 
 .get(routeHelpers.isLoggedIn, wrapAsync(async function(req, res) {
 
-    if (req.user.id !== req.params.source_id)
+    if (req.user.id != req.params.source_id)
         res.status(403).send({ message: 'User is not authorized to perform this request' });
     else {
         let preferences = await db.Preferences.findOne({
@@ -18,20 +18,30 @@ router.route('/preferences/:source_id')
             }
         });
 
-        res.send(preferences);
+        let result = preferences ? preferences.preferencesBlob : '{}';
+        res.send(result);
     }
 }))
 
 .post(wrapAsync(async function(req, res) {
-    if (req.user.id !== req.params.source_id)
+
+    if (req.user.id != req.params.source_id)
         res.status(403).send({ message: 'User is not authorized to perform this request' });
     else {
-        await db.Preferences.update({
-            ...reqBody
-        }, {
+
+        let results = await db.Preferences.findOrCreate({
             where: {
                 sourceId: req.params.source_id
             }
+        });
+
+        if (results[1]) {
+            let authUser = await db.Source.findByPk(req.user.id);
+            results[0].setSource(authUser);
+        }
+
+        await results[0].update({
+            preferencesBlob: req.body.preferences
         });
 
         res.send({ message: 'Preferences are updated' });
