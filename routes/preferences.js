d662f2dfe67cb21d488fe818ee1b6c0be2866468
Farnaz Jahanbakhsh <farnaz.jahanbakhsh92@gmail.com>
2021-06-14 17:34:29 +0430

    update preference fetching and setting

--- routes/preferences.js
+++ routes/preferences.js
@@ -5,47 +5,38 @@ var db  = require('../models');
 var routeHelpers = require('../lib/routeHelpers');
 var wrapAsync = require('../lib/wrappers').wrapAsync;
 
-router.route('/preferences/:source_id')
+router.route('/preferences')
 
 .get(routeHelpers.isLoggedIn, wrapAsync(async function(req, res) {
 
-    if (req.user.id != req.params.source_id)
-        res.status(403).send({ message: 'User is not authorized to perform this request' });
-    else {
-        let preferences = await db.Preferences.findOne({
-            where: {
-                sourceId: req.params.source_id
-            }
-        });
+    let preferences = await db.Preferences.findOne({
+        where: {
+            sourceId: req.user.id
+        }
+    });
 
-        let result = preferences ? preferences.preferencesBlob : '{}';
-        res.send(result);
-    }
+    let result = preferences ? preferences.preferencesBlob : '{}';
+    res.send(result);
 }))
 
-.post(wrapAsync(async function(req, res) {
+.post(routeHelpers.isLoggedIn, wrapAsync(async function(req, res) {
 
-    if (req.user.id != req.params.source_id)
-        res.status(403).send({ message: 'User is not authorized to perform this request' });
-    else {
-
-        let results = await db.Preferences.findOrCreate({
-            where: {
-                sourceId: req.params.source_id
-            }
-        });
-
-        if (results[1]) {
-            let authUser = await db.Source.findByPk(req.user.id);
-            results[0].setSource(authUser);
+    let results = await db.Preferences.findOrCreate({
+        where: {
+            sourceId: req.user.id
         }
+    });
 
-        await results[0].update({
-            preferencesBlob: req.body.preferences
-        });
-
-        res.send({ message: 'Preferences are updated' });
+    if (results[1]) {
+        let authUser = await db.Source.findByPk(req.user.id);
+        results[0].setSource(authUser);
     }
+
+    await results[0].update({
+        preferencesBlob: req.body.preferences
+    });
+
+    res.send({ message: 'Preferences are updated' });
 
 }));
   
