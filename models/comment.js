67d2415c05d8a4bab8a0597e30062d78a3380d14
MCBlastoise <ayyubabdul01@gmail.com>
2021-12-03 15:21:42 -0500

    Endpoint and model changes in preparation of lazy loading feature

--- models/comment.js
+++ models/comment.js
@@ -11,6 +11,21 @@ module.exports = (sequelize, DataTypes) => {
     },
     setId: {
         type: DataTypes.UUID
+    },
+    parentType: {
+        type: DataTypes.INTEGER
+    },
+    parentId: {
+        type: DataTypes.INTEGER
+    },
+    parentSetId: {
+        type: DataTypes.UUID
+    },
+    rootType: {
+        type: DataTypes.INTEGER
+    },
+    rootSetId: {
+        type: DataTypes.UUID // Actually either a comment setId (uuid) or an assessment sourceId (integer)
     }
   }, {
     charset: 'utf8mb4',
@@ -18,8 +33,12 @@ module.exports = (sequelize, DataTypes) => {
 
 
   Comment.associate = function (models) {
-    models.Comment.belongsTo(models.Comment, { as: 'ParentComment' });
-    models.Comment.belongsTo(models.Assessment, { as: 'ParentAssessment' });
+    // models.Comment.belongsTo(models.Comment, { as: 'ParentComment' });
+    // models.Comment.belongsTo(models.Assessment, { as: 'ParentAssessment' });
+
+    // models.Comment.belongsTo(models.Comment, { as: 'RootComment' });
+    // models.Comment.belongsTo(models.Assessment, { as: 'RootAssessment' });
+
     models.Comment.belongsTo(models.Post);
     models.Comment.belongsTo(models.Source);
   };
--- routes/comments.js
+++ routes/comments.js
@@ -45,7 +45,7 @@ expects req.body of the form:
         res.status(403).send({ message: 'Comment set does not exist or does not belong to the user' });
     }
     else {
-        let baseComment = prevComments[0];
+        let baseComment = prevComments[0]; // Pick an arbitrary comment from the set to extract the transitive associations and shared properties
 
         let prevCommentsProms = prevComments.map(comment => {
             return comment.update({
@@ -54,34 +54,58 @@ expects req.body of the form:
         })
 
         let commentProm = db.Comment.create({
-            setId: req.params.set_id,
+            body: req.body.body,
             version: 1,
-            body: req.body.body
+            setId: req.params.set_id,
+            createdAt: baseComment.createdAt,
+            parentType: baseComment.parentType,
+            parentId: baseComment.parentId,
+            parentSetId: baseComment.parentSetId,
+            rootType: baseComment.rootType,
+            rootSetId: baseComment.rootSetId
         });
 
         let authUserProm = db.Source.findByPk(req.user.id);
 
-        let parentInstanceProm = new Promise((resolve) => resolve());
+        let [comment, authUser] = await Promise.all([ commentProm, authUserProm ]);
 
-        if (baseComment.ParentCommentId != null) {
-            parentInstanceProm = db.Comment.findByPk(baseComment.ParentCommentId);
-        }
-        else if (baseComment.ParentAssessmentId != null) {
-            parentInstanceProm = db.Assessment.findByPk(baseComment.ParentAssessmentId);
-        }
+        await Promise.all([ comment.setSource(authUser), comment.setPost(baseComment.Post), prevCommentsProms ]);
 
-        let [comment, authUser, parentInstance] = await Promise.all([ commentProm, authUserProm, parentInstanceProm ]);
+        // let parentInstanceProm = new Promise((resolve) => resolve());
+        // if (baseComment.ParentCommentId !== null) {
+        //     parentInstanceProm = db.Comment.findByPk(baseComment.ParentCommentId);
+        // }
+        // else if (baseComment.ParentAssessmentId !== null) {
+        //     parentInstanceProm = db.Assessment.findByPk(baseComment.ParentAssessmentId);
+        // }
 
-        let parentAssociationProm = new Promise((resolve) => resolve());
+        // let rootInstanceProm = new Promise((resolve) => resolve());
+        // if (baseComment.RootCommentId !== null) {
+        //     rootInstanceProm = db.Comment.findByPk(baseComment.RootCommentId);
+        // }
+        // else if (baseComment.RootAssessmentId !== null) {
+        //     rootInstanceProm = db.Assessment.findByPk(baseComment.RootAssessmentId);
+        // }
 
-        if (baseComment.ParentCommentId != null) {
-            parentAssociationProm = comment.setParentComment(parentInstance);
-        }
-        else if (baseComment.ParentAssessmentId != null) {
-            parentAssociationProm = comment.setParentAssessment(parentInstance);
-        }
+        // let [comment, authUser, parentInstance, rootInstance] = await Promise.all([ commentProm, authUserProm, parentInstanceProm, rootInstanceProm ]);
 
-        await Promise.all([ comment.setSource(authUser), comment.setPost(baseComment.Post), parentAssociationProm, prevCommentsProms ]);
+        // let parentAssociationProm = new Promise((resolve) => resolve());
+        // if (baseComment.ParentCommentId !== null) {
+        //     parentAssociationProm = comment.setParentComment(parentInstance);
+        // }
+        // else if (baseComment.ParentAssessmentId !== null) {
+        //     parentAssociationProm = comment.setParentAssessment(parentInstance);
+        // }
+
+        // let rootAssociationProm = new Promise((resolve) => resolve());
+        // if (baseComment.RootCommentId !== null) {
+        //     rootAssociationProm = comment.setRootComment(rootInstance);
+        // }
+        // else if (baseComment.RootAssessmentId !== null) {
+        //     rootAssociationProm = comment.setRootAssessment(rootInstance);
+        // }
+
+        // await Promise.all([ comment.setSource(authUser), comment.setPost(baseComment.Post), parentAssociationProm, rootAssociationProm, prevCommentsProms ]);
         res.send( { message: 'Comment edited', data: comment} );
     }
 }))
@@ -91,66 +115,69 @@ deleting a comment destroys the instance but posts a new comment with an empty b
 otherwise the replies would need to be deleted as well
 */
 .delete(routeHelpers.isLoggedIn, wrapAsync(async function(req, res) {
+    const [COMMENT_TYPE, ASSESSMENT_TYPE] = [constants.COMMENT_ASSESSMENT_TYPES.COMMENT, constants.COMMENT_ASSESSMENT_TYPES.ASSESSMENT]
+
     let comments = await db.Comment.findAll({
         where: {
             setId: req.params.set_id,
             SourceId: req.user.id
         }
-    });
+    }); // All comments in the selected set
 
     if (!comments.length) {
         res.status(403).send({ message: 'Comment set does not exist or does not belong to the user' });
     }
     else {
-        let baseComment = comments[0];
+        let baseComment = comments[0]; // Pick an arbitrary comment from the set to extract the transitive associations and shared properties
         let dummyCommentProm = db.Comment.create({
-            setId: uuidv4(),
             version: 1,
+            setId: uuidv4(),
+            createdAt: baseComment.createdAt,
+            parentType: baseComment.parentType,
+            parentId: baseComment.parentId,
+            parentSetId: baseComment.parentSetId,
+            rootType: baseComment.rootType,
+            rootSetId: baseComment.rootSetId
         });
 
-        let commentIds = comments.map(el => el.id);
+        let commentIds = comments.map(el => el.id); // The ids of all comments in the selected set
 
-        let [childComments, childAssessments, dummyComment] = await Promise.all([ db.Comment.findAll({
+        let [childComments, dummyComment] = await Promise.all([ db.Comment.findAll({
             where: {
-                ParentCommentId: {
-                    [Op.in]: commentIds
-                }
-            }
-        }),
-        db.Assessment.findAll({
-            where: {
-                ParentCommentId: {
+                parentType: COMMENT_TYPE,
+                parentId: {
                     [Op.in]: commentIds
                 }
             }
         }),
         dummyCommentProm ]);
 
-        if (baseComment.ParentCommentId != null)
-            parentInstanceProm = db.Comment.findByPk(baseComment.ParentCommentId);
-        else if (baseComment.ParentAssessmentId != null)
-            parentInstanceProm = db.Assessment.findByPk(baseComment.ParentAssessmentId);
-        else
-            parentInstanceProm = new Promise((resolve) => resolve());
+        // if (baseComment.ParentCommentId != null)
+        //     parentInstanceProm = db.Comment.findByPk(baseComment.ParentCommentId);
+        // else if (baseComment.ParentAssessmentId != null)
+        //     parentInstanceProm = db.Assessment.findByPk(baseComment.ParentAssessmentId);
+        // else
+        //     parentInstanceProm = new Promise((resolve) => resolve());
 
-        let [commentPost, commentSource, commentParent] = await Promise.all([
+        let [commentPost, commentSource] = await Promise.all([
             db.Post.findByPk(baseComment.PostId),
-            db.Source.findByPk(baseComment.SourceId),
-            parentInstanceProm
+            db.Source.findByPk(baseComment.SourceId)
         ])
 
-        if (baseComment.ParentCommentId != null)
-            parentAssociationProm = dummyComment.setParentComment(commentParent);
-        else if (baseComment.ParentAssessmentId != null)
-            parentAssociationProm = dummyComment.setParentAssessment(commentParent);
-        else
-            parentAssociationProm = new Promise((resolve) => resolve());
+        // if (baseComment.ParentCommentId != null)
+        //     parentAssociationProm = dummyComment.setParentComment(commentParent);
+        // else if (baseComment.ParentAssessmentId != null)
+        //     parentAssociationProm = dummyComment.setParentAssessment(commentParent);
+        // else
+        //     parentAssociationProm = new Promise((resolve) => resolve());
 
-        await Promise.all([dummyComment.setPost(commentPost), dummyComment.setSource(commentSource), parentAssociationProm]);
+        await Promise.all([ dummyComment.setPost(commentPost), dummyComment.setSource(commentSource) ]);
 
-        let children = [...childComments, ...childAssessments];
-        let associationProms = children.map(child => {
-            child.setParentComment(dummyComment)
+        let associationProms = childComments.map(child => {
+            child.update({
+                'parentId': dummyComment.id,
+                'parentSetId': dummyComment.setId
+            });
         })
 
         let destroyProms = comments.map(el => el.destroy());
@@ -176,7 +203,8 @@ router.route('/comments/posts/:post_id')
                     [Op.eq]: post.SourceId,
                     [Op.in]: viewableSources
                   }
-            }
+            },
+            version: 1
         },
         include: {
             model: db.Source
@@ -197,6 +225,8 @@ expects req.body of the form:
 */
 .post(routeHelpers.isLoggedIn, wrapAsync(async function(req, res) {
 
+    const [COMMENT_TYPE, ASSESSMENT_TYPE] = [constants.COMMENT_ASSESSMENT_TYPES.COMMENT, constants.COMMENT_ASSESSMENT_TYPES.ASSESSMENT]
+
     let commentProm = db.Comment.create({
         body: req.body.body,
         setId: uuidv4(),
@@ -209,28 +239,38 @@ expects req.body of the form:
     let parentInstanceProm = new Promise((resolve) => resolve());
 
     if (req.body.repliesTo) { //if the comment is not a top level comment
-        if (req.body.repliesToType == constants.COMMENT_ASSESSMENT_TYPES.COMMENT) {
-            parentInstanceProm = db.Comment.findByPk(req.body.repliesTo);
-        }
-        else {
-            parentInstanceProm = db.Assessment.findByPk(req.body.repliesTo);
-        }
+        parentInstanceProm = (req.body.repliesToType == COMMENT_TYPE) ?
+                             db.Comment.findByPk(req.body.repliesTo) :
+                             db.Assessment.findByPk(req.body.repliesTo);
     }
 
     let [comment, authUser, post, parentInstance] = await Promise.all([ commentProm, authUserProm, postProm, parentInstanceProm ]);
 
     let parentAssociationProm = new Promise((resolve) => resolve());
+    let rootAssocationProm = new Promise((resolve) => resolve());
 
     if (req.body.repliesTo) { //if the comment is not a top level comment
-        if (req.body.repliesToType == constants.COMMENT_ASSESSMENT_TYPES.COMMENT) {
-            parentAssociationProm = comment.setParentComment(parentInstance);
+        parentAssociationProm = comment.update({
+                'parentType': parseInt(req.body.repliesToType),
+                'parentId': parentInstance.id,
+                'parentSetId': req.body.repliesToType == COMMENT_TYPE ? parentInstance.setId : parentInstance.SourceId
+        });
+
+        if (req.body.repliesToType == ASSESSMENT_TYPE || parentInstance.rootType === null) { // Replied to a top-level, root is same as parent
+            rootAssociationProm = comment.update({
+                'rootType': parseInt(req.body.repliesToType),
+                'rootSetId': req.body.repliesToType == COMMENT_TYPE ? parentInstance.setId : parentInstance.SourceId
+            });
         }
-        else {
-            parentAssociationProm = comment.setParentAssessment(parentInstance);
+        else { // Replied to a non-top-level, root is same as root of parent
+            rootAssociationProm = comment.update({
+                'rootType': parentInstance.rootType,
+                'rootSetId': parentInstance.rootSetId
+            });
         }
     }
 
-    await Promise.all( [comment.setSource(authUser), comment.setPost(post), parentAssociationProm ]);
+    await Promise.all( [comment.setSource(authUser), comment.setPost(post), parentAssociationProm, rootAssocationProm ]);
     res.send({ message: 'Comment posted', data: comment })
 }));
 
