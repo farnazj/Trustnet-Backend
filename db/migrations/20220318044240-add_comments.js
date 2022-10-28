38f9bc561f45be507da5f30e090377713cd4f101
farnazj@mit.edu <farnaz.jahanbakhsh92@gmail.com>
2022-10-28 19:05:13 -0400

    Undo commenting migration

--- db/migrations/20220318044240-add_comments.js
+++ db/migrations/20220318044240-add_comments.js
@@ -5,64 +5,64 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return new Promise(async (resolve, reject) => {
       try {
-        // await queryInterface.createTable('Comments', {
-        //   id: {
-        //     type: Sequelize.INTEGER,
-        //     primaryKey: true,
-        //     autoIncrement: true
-        //   },
-        //   body: {
-        //     type: Sequelize.TEXT('long'),
-        //   },
-        //   version: {
-        //     type: Sequelize.INTEGER,
-        //     defaultValue: 1
-        //   },
-        //   setId: {
-        //     type: Sequelize.UUID
-        //   },
-        //   parentType: {
-        //     type: Sequelize.INTEGER
-        //   },
-        //   parentId: {
-        //     type: Sequelize.INTEGER
-        //   },
-        //   parentSetId: {
-        //       type: Sequelize.UUID
-        //   },
-        //   rootType: {
-        //       type: Sequelize.INTEGER
-        //   },
-        //   rootSetId: {
-        //       type: Sequelize.UUID
-        //   },
-        //   createdAt: {
-        //     type: Sequelize.DATE
-        //   },
-        //   updatedAt: {
-        //     type: Sequelize.DATE
-        //   },
-        //   PostId: {
-        //     type: Sequelize.INTEGER,
-        //     references: {
-        //       model: 'Posts',
-        //       key: 'id'
-        //     },
-        //     onUpdate: 'cascade',
-        //     onDelete: 'cascade'
-        //   },
-        //   SourceId: {
-        //     type: Sequelize.INTEGER,
-        //     references: {
-        //       model: 'Sources',
-        //       key: 'id'
-        //     },
-        //     onUpdate: 'cascade',
-        //     onDelete: 'cascade'
-        //   },
-        // }, {
-        //   charset: 'utf8mb4'
-        // });
+        await queryInterface.createTable('Comments', {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          body: {
+            type: Sequelize.TEXT('long'),
+          },
+          version: {
+            type: Sequelize.INTEGER,
+            defaultValue: 1
+          },
+          setId: {
+            type: Sequelize.UUID
+          },
+          parentType: {
+            type: Sequelize.INTEGER
+          },
+          parentId: {
+            type: Sequelize.INTEGER
+          },
+          parentSetId: {
+              type: Sequelize.UUID
+          },
+          rootType: {
+              type: Sequelize.INTEGER
+          },
+          rootSetId: {
+              type: Sequelize.UUID
+          },
+          createdAt: {
+            type: Sequelize.DATE
+          },
+          updatedAt: {
+            type: Sequelize.DATE
+          },
+          PostId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'Posts',
+              key: 'id'
+            },
+            onUpdate: 'cascade',
+            onDelete: 'cascade'
+          },
+          SourceId: {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'Sources',
+              key: 'id'
+            },
+            onUpdate: 'cascade',
+            onDelete: 'cascade'
+          },
+        }, {
+          charset: 'utf8mb4'
+        });
 
         resolve();
 
